869cf89c6a02bda894edf8d50c64f5d7f079ec2b
Kenley Arai <Karai001@ucr.edu>
2018-02-28 18:03:20 -0800

    Added MOCK data, and PropTypes for the default App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+import PropTypes from "prop-types";
 
 import OutputView from "../components/OutputView";
 import InputView from "../components/InputView";
 
+import data from "../../MOCK_DATA.json";
+
 export default class App extends Component<{}> {
+  static defaultProps = { heart_samples: data };
+
+  static propTypes = {
+    heart_samples: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.number,
+        timestamp: PropTypes.string
+      })
+    )
+  };
+
   render() {
     return (
       <View>
