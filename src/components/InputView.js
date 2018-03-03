eed4b5c4afd95a6498d2b47aac9b76172669a5a6
Kenley Arai <Karai001@ucr.edu>
2018-03-02 16:19:52 -0800

    Cleaned up InputView to display based on the number of levels input

--- src/components/InputView.js
+++ src/components/InputView.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
-
+import PropTypes from "prop-types";
 import Button from "./Button";
 
 export default class InputView extends Component<{}> {
+  static propTypes = {
+    levels: PropTypes.arrayOf(PropTypes.number)
+  };
+
+  static defaultProps = {
+    levels: [1, 2, 3]
+  };
+
   render() {
+    let { levels } = { ...this.props };
+
     return (
-      <View>
-        <Button />
-        <Button />
-        <Button />
-      </View>
+      <View>{levels.map(level => <Button key={level} level={level} />)}</View>
     );
   }
 }
