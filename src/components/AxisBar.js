c778786603222e3c9072ead40ba5436e3f9eaedf
Kenley Arai <Karai001@ucr.edu>
2018-03-03 14:48:37 -0800

    Added X and Y axis

--- src/components/AxisBar.js
+++ src/components/AxisBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
+import { YAxis, XAxis } from "react-native-svg-charts";
 import PropTypes from "prop-types";
 
 import Bar from "./Bar";
@@ -7,7 +8,12 @@ import VerticalBar from "./VerticalBar";
 
 export default class AxisBar extends Component<{}> {
   static propTypes = {
-    data: PropTypes.arrayOf(PropTypes.object),
+    data: PropTypes.arrayOf(
+      PropTypes.shape({
+        value: PropTypes.number,
+        timestamp: PropTypes.string
+      })
+    ),
     is_vertical: PropTypes.bool
   };
 
@@ -16,10 +22,22 @@ export default class AxisBar extends Component<{}> {
   };
 
   render() {
-    return this.props.is_vertical == true ? (
-      <Bar data={this.props.data} />
+    let { data, is_vertical } = { ...this.props };
+
+    return is_vertical ? (
+      <YAxis
+        data={data.map(d => d["value"])}
+        svg={{
+          fill: "grey",
+          fontSize: 10
+        }}
+        formatLabel={value => `${value}`}
+      />
     ) : (
-      <VerticalBar data={this.props.data} />
+      <XAxis
+        style={{ marginHorizontal: -10 }}
+        data={data.map(d => d["timestamp"])}
+      />
     );
   }
 }
